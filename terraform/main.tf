21581863f444f20456b680e70f8cc4f76cd32a57
Phillip Urrea <phillip@nestifi.money>
2026-03-22 20:08:16 +0000

    fixed url for api

--- terraform/main.tf
+++ terraform/main.tf
@@ -501,7 +501,7 @@ resource "google_cloud_run_v2_service" "admin" {
 
       env {
         name  = "NEXT_PUBLIC_API_URL"
-        value = google_cloud_run_v2_service.api.uri
+        value = "${google_cloud_run_v2_service.api.uri}/api"
       }
 
       env {
